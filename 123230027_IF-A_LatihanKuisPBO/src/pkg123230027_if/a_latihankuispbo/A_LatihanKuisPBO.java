51795a2c6546aa5d3619581b237ba56524981aa3
ARIL <150516410+begalskuter@users.noreply.github.com>
2025-03-15 11:03:53 +0700

    Add files via upload

---
+++ 123230027_IF-A_LatihanKuisPBO/src/pkg123230027_if/a_latihankuispbo/A_LatihanKuisPBO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg123230027_if.a_latihankuispbo;
+
+/**
+ *
+ * @author Asus
+ */
+public class A_LatihanKuisPBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new Login();
+    }
+    
+}
